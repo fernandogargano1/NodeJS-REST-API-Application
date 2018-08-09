6a2c622484e7047010be7ffb463d5ee7714a7087
Fernando Gargano <fernando@google.com>
2018-08-09 16:14:51 +0200

    Add Integration Test for DELETE route handler

--- routes/genres.js
+++ routes/genres.js
@@ -34,7 +34,7 @@ router.put('/:id',  [validateObjectId, auth], async (req, res) => {
   res.send(genre);
 });
 
-router.delete('/:id', [auth, admin], async (req, res) => {
+router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
