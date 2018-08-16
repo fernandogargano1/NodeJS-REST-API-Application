0e590c8da8ac24b52366da4d2064cfe9dc1a9653
Fernando Gargano <fernando@google.com>
2018-08-16 12:18:04 +0200

    Add functionality with more Test-Driven Development POST /api/returns

--- routes/returns.js
+++ routes/returns.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { Rental } = require('../models/rental');
 
 const auth = require('../middleware/auth');
 
-router.post('/', auth, async (req, res) => {    
+router.post('/', async (req, res) => {    
 
     if (!req.body.customerId) return res.status(400).send('customerId not provided');
 
     if (!req.body.movieId) return res.status(400).send('movieId not provided');
 
-    return res.status(401).send('Unauthorized');    
+    const rental =  await Rental.findOne({
+        'customer._id': req.body.customerId, 
+        'movie._id': req.body.movieId 
+    });
+
+    console.log('rental: ', rental);
+
+    if (!rental) return res.status(404).send('Rental not found');
+
+    if (rental.dateReturned) res.status(400).send('Returned already processed'); 
+
+    return res.status(401).send('Unauthorized'); 
+   
 });
 
 module.exports = router;
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
 
-const {Rental} = require('../../models/rental');
+const { Rental } = require('../../models/rental');
 const { User } = require('../../models/user');
 
 describe('/api/returns', () => {
@@ -9,12 +9,23 @@ describe('/api/returns', () => {
     let customerId;
     let movieId;
     let rental;
+    let token;
+
+    // Let's define the happy path 
+    const exec = () => {
+        return request(server)            
+            .post('/api/returns')
+            .set('x-auth-token', token)
+            .send({ customerId, movieId });;   
+    }
 
     beforeEach(async () => { 
         server = require('../../index'); 
 
+        // These are valid values for the happy path
         customerId = mongoose.Types.ObjectId();
         movieId = mongoose.Types.ObjectId();
+        token = new User().generateAuthToken();
 
         rental = new Rental({
             customer: {
@@ -37,35 +48,55 @@ describe('/api/returns', () => {
     });
 
     it('should return 401 if client is not logged in', async () => {       
+        token = '';
 
-        const res = await request(server)
-            .post('/api/returns')                                   
-            .send({ customerId, movieId });
+        const res = await exec();
 
         expect(res.status).toBe(401);    
     });
 
     it('should return 400 if customerId is not provided', async () => {
         
-        const token = new User().generateAuthToken();
+       /* Another alternative is to defined an object payload and 
+          delete customer id 
 
-        const res = await request(server)            
-            .post('/api/returns')
-            .set('x-auth-token', token)
-            .send({ movieId });;           
+                delete payload.customerId
+
+          Either way if we don't have a customer id in the code of a request
+          our current implementation is going to return a 400 error
+       */
+       customerId = '';
+                
+       const res = await exec();
             
         expect(res.status).toBe(400);
-    })
+    });
 
     it('should return 400 if movieId is not provided', async () => {
         
-        const token = new User().generateAuthToken();
-
-        const res = await request(server)            
-            .post('/api/returns')
-            .set('x-auth-token', token)
-            .send({ customerId });;           
+        movieId = '';
+        
+        const res = await exec();
             
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 404 is not rental found for the customer/movie', async () => {
+
+        await Rental.remove({});       
+
+        const res = await exec();
+
+        expect(res.status).toBe(404);
+    });
+
+    it('should return 400 is rental already processed', async () => {      
+        
+        rental.dateReturned = Date();
+        await rental.save();
+
+        const res = await exec();
+
         expect(res.status).toBe(400);
     })
 });
